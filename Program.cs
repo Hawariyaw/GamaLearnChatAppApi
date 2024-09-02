03fe2fadc33b9a7d931d0920f10b14229d00f42a
Hawi Paul <hawariyaw.pawulos@resultier.com>
2024-09-02 13:40:02 +0400

    fix: expose swagger

--- Program.cs
+++ Program.cs
@@ -95,11 +95,10 @@ builder.Services.AddScoped<IMessageRepository, MessageRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
